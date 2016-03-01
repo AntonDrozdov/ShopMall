77458a662cc609f42560075baf8bdbb721b33d11
Anton.Drozdov <Anton.Drozdov@astrosoft.ru>
2016-03-01 10:51:23 +0300

    initial

---
+++ src/NewShopMall/DBContexts/ShopMallDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using NewShopMall.Models.ShopMallDBModels;
+
+namespace NewShopMall.DBContexts
+{
+    public class ShopMallDBContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
---
+++ src/NewShopMall/Models/ShopMallDBModels/Shop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewShopMall.Models.ShopMallDBModels
+{
+    public class Shop
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+    }
+}
